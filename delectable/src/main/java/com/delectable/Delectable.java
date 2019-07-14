ee5d05269590caf2dc2405f9f23c2898f2dd8388
Rodney <rodneyll@umich.edu>
2019-07-14 07:50:52 -0400

    Initial commit

---
+++ delectable/src/main/java/com/delectable/Delectable.java
@@ -0,0 +1,13 @@
+package com.delectable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Delectable{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Delectable.class, args);
+	}
+
+}
