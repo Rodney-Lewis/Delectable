afb87cb03e94cb365b4f83091e0e16840c2247de
Rodney Lewis <rodneylewis95@gmail.com>
2020-11-27 09:28:52 -0500

    File moves

--- delectable/src/main/java/com/delectable/conf/ConfigurationService.java
+++
@@ -1,8 +0,0 @@
-package com.delectable.conf;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConfigurationService extends CrudRepository<Configuration, Integer> {
-}
