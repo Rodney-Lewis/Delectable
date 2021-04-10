510e9253ac3624647ef996bab85971bb18862adf
Rodney Lewis <rodneylewis95@gmail.com>
2021-04-10 08:24:53 -0400

    Fixed schedule services

--- delectable/src/main/java/com/delectable/schedule/ScheduleController.java
+++ delectable/src/main/java/com/delectable/schedule/ScheduleController.java
@@ -3,20 +3,16 @@ package com.delectable.schedule;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import com.delectable.combo.Combo;
-import com.delectable.combo.ComboRepository;
+import com.delectable.combo.ComboService;
 import com.delectable.recipe.Recipe;
 import com.delectable.recipe.RecipeService;
 import com.delectable.restaurant.Restaurant;
-import com.delectable.restaurant.RestaurantRepository;
+import com.delectable.restaurant.RestaurantService;
 import com.delectable.shared.crud.CRUHardDeleteController;
 import com.delectable.shared.crud.CRUHardDeleteRepository;
 import com.delectable.shared.crud.CRUSoftDeleteRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,80 +25,71 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class ScheduleController extends CRUHardDeleteController<Schedule> {
 
-    public ScheduleController(CRUHardDeleteRepository<Schedule> repository,
-            CRUSoftDeleteRepository<Recipe> Rrepository, CRUSoftDeleteRepository<Restaurant> Rreporrsitory, CRUSoftDeleteRepository<Combo> Rrepositrrory) {
-        super(repository);
-        recipeRepository = new RecipeService(Rrepository);
+  public ScheduleController(CRUHardDeleteRepository<Schedule> scheduleRepository,
+      CRUSoftDeleteRepository<Recipe> recipeRepository,
+      CRUSoftDeleteRepository<Restaurant> restaurantRepository,
+      CRUSoftDeleteRepository<Combo> comboRepository) {
+    super(scheduleRepository);
+    scheduleService = new ScheduleService(scheduleRepository);
+    recipeService = new RecipeService(recipeRepository);
+    restaurantService = new RestaurantService(restaurantRepository);
+    comboService = new ComboService(comboRepository);
+  }
+
+  ScheduleService scheduleService;
+  RecipeService recipeService;
+  RestaurantService restaurantService;
+  ComboService comboService;
+
+  @GetMapping
+  ResponseEntity<List<List<Schedule>>> getScheduledBetween(@RequestParam String begin,
+      @RequestParam String end) {
+
+    List<Schedule> scheduled = new ArrayList<Schedule>();
+    scheduled.addAll(scheduleService.findByEpochBetweenOrderByEpochAsc(Long.parseLong(begin),
+        Long.parseLong(end)));
+    List<List<Schedule>> scheduledByDate = new ArrayList<List<Schedule>>();
+
+    Calendar date1 = Calendar.getInstance();
+    Calendar date2 = Calendar.getInstance();
+    date1.setTimeInMillis(Long.parseLong(begin));
+    date2.setTimeInMillis(Long.parseLong(end));
+
+    var timeBetweenDates = Math.abs(date1.getTimeInMillis() - date2.getTimeInMillis());
+    var daysBetweenDates = Math.floor(timeBetweenDates / (24 * 3600 * 1000));
+    List<Date> dates = new ArrayList<Date>();
+
+    for (int i = 0; i < daysBetweenDates + 1; i++) {
+      dates.add(date1.getTime());
+      date1.add(Calendar.DATE, 1);
     }
 
-    @Autowired
-    ScheduleRepository scheduleService;
-
-    RecipeService recipeRepository;
-
-    @Autowired
-    RestaurantRepository restaurantRepository;
-
-    @Autowired
-    ComboRepository comboRepository;
-
-    @GetMapping
-    ResponseEntity<Map<String, Object>> getScheduledBetween(@RequestParam String begin,
-            @RequestParam String end) {
-
-        List<Schedule> scheduled = new ArrayList<Schedule>();
-        scheduled.addAll(scheduleService.findByEpochBetweenOrderByEpochAsc(Long.parseLong(begin),
-                Long.parseLong(end)));
-        Map<String, Object> response = new HashMap<>();
-        List<List<Schedule>> scheduledByDate = new ArrayList<List<Schedule>>();
-
-        Calendar date1 = Calendar.getInstance();
-        Calendar date2 = Calendar.getInstance();
-        date1.setTimeInMillis(Long.parseLong(begin));
-        date2.setTimeInMillis(Long.parseLong(end));
-
-        var timeBetweenDates = Math.abs(date1.getTimeInMillis() - date2.getTimeInMillis());
-        var daysBetweenDates = Math.floor(timeBetweenDates / (24 * 3600 * 1000));
-        List<Date> dates = new ArrayList<Date>();
-
-        for (int i = 0; i < daysBetweenDates + 1; i++) {
-            dates.add(date1.getTime());
-            date1.add(Calendar.DATE, 1);
+    for (int k = 0; k < dates.size(); k++) {
+      scheduledByDate.add(new ArrayList<Schedule>());
+      for (int j = 0; j < scheduled.size(); j++) {
+        if (dates.get(k).getTime() == scheduled.get(j).epoch) {
+          Schedule t = scheduled.get(j);
+          if (t.getScheduleType() == ScheduleType.RECIPE) {
+            t.setScheduledItemName(recipeService.get(t.getScheduledItemId()).getName());
+          } else if (t.getScheduleType() == ScheduleType.RESTAURANT) {
+            t.setScheduledItemName(restaurantService.get(t.getScheduledItemId()).getName());
+          } else if (t.getScheduleType() == ScheduleType.COMBO) {
+            t.setScheduledItemName(comboService.get(t.getScheduledItemId()).getName());
+          }
+          scheduledByDate.get(k).add(scheduled.get(j));
         }
-
-        for (int k = 0; k < dates.size(); k++) {
-            scheduledByDate.add(new ArrayList<Schedule>());
-            for (int j = 0; j < scheduled.size(); j++) {
-                if (dates.get(k).getTime() == scheduled.get(j).epoch) {
-                    Schedule t = scheduled.get(j);
-
-                    if (t.getScheduleType() == ScheduleType.RECIPE) {
-                        t.setScheduledItemName(
-                                recipeRepository.get(t.getScheduledItemId()).getName());
-                    } else if (t.getScheduleType() == ScheduleType.RESTAURANT) {
-                        t.setScheduledItemName(restaurantRepository.findById(t.getScheduledItemId())
-                                .get().getName());
-                    } else if (t.getScheduleType() == ScheduleType.COMBO) {
-                        t.setScheduledItemName(
-                                comboRepository.findById(t.getScheduledItemId()).get().getName());
-                    }
-                    scheduledByDate.get(k).add(scheduled.get(j));
-                }
-            }
-        }
-        response.put("content", scheduledByDate);
-        response.put("totalElements", scheduled.size());
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
+      }
     }
+    return ResponseEntity.ok().body(scheduledByDate);
+  }
 
-    @GetMapping("/mealtypes")
-    MealTime[] getMealtypes() {
-        return MealTime.values();
-    }
+  @GetMapping("/mealtypes")
+  MealTime[] getMealtypes() {
+    return MealTime.values();
+  }
 
-    @GetMapping("/scheduletypes")
-    ScheduleType[] getScheduleTypes() {
-        return ScheduleType.values();
-    }
+  @GetMapping("/scheduletypes")
+  ScheduleType[] getScheduleTypes() {
+    return ScheduleType.values();
+  }
 }
--- delectable/src/main/java/com/delectable/schedule/ScheduleService.java
+++ delectable/src/main/java/com/delectable/schedule/ScheduleService.java
@@ -1,10 +1,20 @@
 package com.delectable.schedule;
 
+import java.util.List;
 import com.delectable.shared.crud.CRUHardDeleteRepository;
 import com.delectable.shared.crud.CRUHardDeleteService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class ScheduleService extends CRUHardDeleteService<Schedule> {
-    public ScheduleService(CRUHardDeleteRepository<Schedule> repository) {
-        super(repository);
+
+  @Autowired
+  ScheduleRepository scheduleRepository;
+
+  public ScheduleService(CRUHardDeleteRepository<Schedule> repository) {
+    super(repository);
+  }
+
+  public List<Schedule> findByEpochBetweenOrderByEpochAsc(Long start, Long end) {
+      return scheduleRepository.findByEpochBetweenOrderByEpochAsc(start, end);
     }
 }
